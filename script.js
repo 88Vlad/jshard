fc8ed7e5809922e776114e18c6554e730ba93d86
Vlad <vl@mair.tail68326.ts.net>
2024-06-17 23:55:17 +0200

    урок 7 hard changed

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 'use strict'
-// массив week и записать в него дни недели в виде строк
+// массив week
 const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 // текущий день недели
 const currentDayIndex = new Date().getDay();
-// контейнер для вывода дней недели
+//вывод дней недели
 const daysContainer = document.getElementById('daysContainer');
 
-// Убедитесь, что контейнер существует
 if (daysContainer) {
     // все дни недели
     week.forEach((day, index) => {
         const dayElement = document.createElement('p');
         dayElement.textContent = day;
 
-        // Выходные дни - курсивом
+        // Выходные дни - курсив
         if (index === 0 || index === 6) {
             dayElement.style.fontStyle = 'italic';
         }
 
-        // Текущий день - жирным шрифтом
+        // Текущий день - жирный
         if (index === currentDayIndex) {
             dayElement.style.fontWeight = 'bold';
         }
 
-        // Добавляем элемент в контейнер
+
         daysContainer.appendChild(dayElement);
     });
 } else {
     console.error('daysContainer not found in the document.');
 }
-
-<
-!DOCTYPE html >
-    ...
